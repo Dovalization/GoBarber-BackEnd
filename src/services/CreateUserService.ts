5496504c5503d07d20684a1a83a2e5317d44c82a
Guilherme Doval <doval.guilherme94@gmail.com>
2020-11-19 20:01:52 -0300

    authentication done

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -3,6 +3,8 @@ import { hash } from 'bcryptjs';
 
 import User from '../models/User';
 
+import AppError from '../errors/AppError';
+
 interface Request {
   name: string;
   email: string;
@@ -18,7 +20,7 @@ class CreatUserService {
     });
 
     if (checkUserExists) {
-      throw new Error('User already exists');
+      throw new AppError('User already exists');
     }
 
     const hashedPassword = await hash(password, 8);
